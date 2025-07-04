136c203d5c0702edd30b7388610af7b75c899204
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 11:09:31 +1200

    Added basic table for mentions for posts

---
+++ server/db/migrations/20250704230820_mentions.js
@@ -0,0 +1,24 @@
+/**
+/**
+ * @param {import('knex').Knex} knex
+ */
+export async function up(knex) {
+  return knex.schema.createTable('mentions', (table) => {
+    table
+      .integer('UserId')
+      .unsigned()
+      .notNullable()
+      .references('Id')
+      .inTable('users')
+    table
+      .integer('PostId')
+      .unsigned()
+      .notNullable()
+      .references('Id')
+      .inTable('posts')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('mentions')
+}
