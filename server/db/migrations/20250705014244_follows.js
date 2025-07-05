4217b72547338de428cbf7af9c165306b5a8f92d
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 13:44:00 +1200

    Added basic migrations table for follows

---
+++ server/db/migrations/20250705014244_follows.js
@@ -0,0 +1,24 @@
+/**
+/**
+ * @param {import('knex').Knex} knex
+ */
+export async function up(knex) {
+  return knex.schema.createTable('follows', (table) => {
+    table
+      .integer('UserId')
+      .unsigned()
+      .notNullable()
+      .references('Id')
+      .inTable('users')
+    table
+      .integer('FollowedUserId')
+      .unsigned()
+      .notNullable()
+      .references('Id')
+      .inTable('users')
+  })
+}
+
+export async function down(knex) {
+  return knex.schema.dropTable('follows')
+}
