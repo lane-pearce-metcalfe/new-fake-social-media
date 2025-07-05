865685b2cb01d1d4b6eae6c765cf3d2f5a19bef4
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-06 10:56:44 +1200

    Added - All exisiting api routes to server file

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,35 @@
 import express from 'express'
 import * as Path from 'node:path'
 
+import commentRoutes from './db/routes/comments.ts'
+import conversationParticipantRoutes from './db/routes/comments.ts'
+import conversationRoutes from './db/routes/conversations.ts'
+import followRoutes from './db/routes/follows.ts'
+import likeRoutes from './db/routes/likes.ts'
+import mentionRoutes from './db/routes/mentions.ts'
+import messageReactionRoutes from './db/routes/messageReactions.ts'
+import messageRoutes from './db/routes/mentions.ts'
+import postRoutes from './db/routes/posts.ts'
+import userProfileRoutes from './db/routes/userProfile.ts'
+import userRoutes from './db/routes/users.ts'
+import userSettingRoutes from './db/routes/userSettings.ts'
 
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/v1/comments', commentRoutes)
+server.use('/api/v1/conversationParticipants', conversationParticipantRoutes)
+server.use('/api/v1/conversations', conversationRoutes)
+server.use('/api/v1/follows', followRoutes)
+server.use('/api/v1/likes', likeRoutes)
+server.use('/api/v1/mentions', mentionRoutes)
+server.use('/api/v1/messageReactions', messageReactionRoutes)
+server.use('/api/v1/messages', messageRoutes)
+server.use('/api/v1/posts', postRoutes)
+server.use('/api/v1/userProfiles', userProfileRoutes)
+server.use('/api/v1/users', userRoutes)
+server.use('/api/v1/userSettings', userSettingRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
