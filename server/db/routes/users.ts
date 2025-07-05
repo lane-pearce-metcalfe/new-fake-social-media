2feccce343c25bdf3c7f8e9850192020615973b5
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 23:19:28 +1200

    Added - Api route for getting a user by their id

---
+++ server/db/routes/users.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+
+import * as db from '../dbFunctions/users.ts'
+
+const router = Router()
+
+router.get('/id/:id', async (req, res) => {
+  const UserId = Number(req.params.id)
+
+  try {
+    const user = await db.getUserById(UserId)
+
+    res.send(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: `Something went wrong grabbing user ${UserId}`,
+    })
+  }
+})
+
+export default router
