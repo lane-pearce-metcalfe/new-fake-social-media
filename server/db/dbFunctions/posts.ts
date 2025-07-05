017231c9f070cd0f1c15ea9fb40757a227c98f44
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 14:08:01 +1200

    Added - Database function to get all posts from a certain user

--- server/db/dbFunctions/posts.ts
+++ server/db/dbFunctions/posts.ts
@@ -4,3 +4,8 @@ export async function getPostById(Id: number) {
   const post = await db('posts').where({ Id }).first()
   return post
 }
+
+export async function getPostsFromUser(UserId: number) {
+  const posts = await db('posts').where({ UserId }).select()
+  return posts
+}
