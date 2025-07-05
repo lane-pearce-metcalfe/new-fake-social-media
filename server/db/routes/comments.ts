748777b42ecc36bb3ab9c6b8c4a404f318aa8859
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 18:31:30 +1200

    Added - Api route for getting all comments from a post

---
+++ server/db/routes/comments.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+
+import * as db from '../dbFunctions/comments'
+
+const router = Router()
+
+router.get('/post/:id', async (req, res) => {
+  const PostId = Number(req.params.id)
+
+  try {
+    const comments = await db.getCommentsOnPost(PostId)
+
+    res.send(200).json(comments)
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({
+        message: `Something went wrong grabbing comments on post ${PostId}`,
+      })
+  }
+})
+
+export default router
