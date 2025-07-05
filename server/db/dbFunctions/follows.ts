80eb92b6c43f14b3864dc7a5be0d30a94f7fc3a4
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 17:27:37 +1200

    Added - Database function to get all the accounts that follow a certain user

--- server/db/dbFunctions/follows.ts
+++ server/db/dbFunctions/follows.ts
@@ -4,3 +4,8 @@ export async function getUsersFollowing(UserId: number) {
   const following = await db('follows').where({ UserId }).select()
   return following
 }
+
+export async function getUsersFollowers(FollowedUserId: number) {
+  const followers = await db('follows').where({ FollowedUserId }).select()
+  return followers
+}
