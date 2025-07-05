08fa069854b2a48c13eb4381185fca0fbb58ed9e
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 18:53:10 +1200

    Added - Api route for getting participants from a conversation id

---
+++ server/db/routes/conversationParticipants.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+
+import * as db from '../dbFunctions/conversationParticipants.ts'
+
+const router = Router()
+
+router.get('/:id', async (req, res) => {
+  const ConversationId = Number(req.params.id)
+
+  try {
+    const participants = await db.getConversationParticipants(ConversationId)
+
+    res.send(200).json(participants)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: `Something went wrong grabbing participants from conversation ${ConversationId}`,
+    })
+  }
+})
+
+export default router
