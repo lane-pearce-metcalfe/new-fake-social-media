fd36e0b20852a3a8cdecab83913d0082befef8b6
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-05 23:16:47 +1200

    Added - Api route for getting user profile info

---
+++ server/db/routes/userProfile.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express'
+
+import * as db from '../dbFunctions/userProfile.ts'
+
+const router = Router()
+
+router.get('/:id', async (req, res) => {
+  const UserId = Number(req.params.id)
+
+  try {
+    const profileInfo = await db.getUsersProfileInfo(UserId)
+
+    res.send(200).json(profileInfo)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: `Something went wrong grabbing profile into for user ${UserId}`,
+    })
+  }
+})
+
+export default router
