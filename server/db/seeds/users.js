620371bf6aa890412287eee342dcc48ca65ee5e5
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-06 12:39:43 +1200

    Added - Seed data for users

---
+++ server/db/seeds/users.js
@@ -0,0 +1,56 @@
+export async function seed(knex) {
+  await knex('users').del()
+
+  await knex('users').insert([
+    {
+      Id: 1,
+      UserName: 'Test UserName 1',
+      FullName: 'Test FullName 1',
+      Email: 'testEmailOne@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: 'Test Date 1',
+      Auth0Sub: 'Auth0|1',
+    },
+    {
+      Id: 2,
+      UserName: 'Test UserName 2',
+      FullName: 'Test FullName 2',
+      Email: 'testEmailTwo@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: 'Test Date 2',
+      Auth0Sub: 'Auth0|2',
+    },
+    {
+      Id: 3,
+      UserName: 'Test UserName 3',
+      FullName: 'Test FullName 3',
+      Email: 'testEmailThree@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: 'Test Date 3',
+      Auth0Sub: 'Auth0|3',
+    },
+    {
+      Id: 4,
+      UserName: 'Test UserName 4',
+      FullName: 'Test FullName 4',
+      Email: 'testEmailFour@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: 'Test Date 4',
+      Auth0Sub: 'Auth0|4',
+    },
+    {
+      Id: 5,
+      UserName: 'Test UserName 5',
+      FullName: 'Test FullName 5',
+      Email: 'testEmailFive@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: 'Test Date 5',
+      Auth0Sub: 'Auth0|5',
+    },
+  ])
+}
