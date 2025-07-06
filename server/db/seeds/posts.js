12f8eb91f9973341bce41e2fcf68913ad1107f52
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-06 13:33:57 +1200

    Added - Seed data for posts

---
+++ server/db/seeds/posts.js
@@ -0,0 +1,30 @@
+export async function seed(knex) {
+  await knex('users').del()
+
+  await knex('users').insert([
+    {
+      Id: 1,
+      UserId: 1,
+      ImgUrl:
+        'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/514000/514749-whistler.jpg',
+      CreatedAt: 'Test date 1',
+      Location: 'Whistler, Canada',
+    },
+    {
+      Id: 2,
+      UserId: 2,
+      ImgUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_août_2014_%282%29.jpg/960px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_août_2014_%282%29.jpg',
+      CreatedAt: 'Test date 2',
+      Loaction: 'Paris, France',
+    },
+    {
+      Id: 3,
+      UserId: 2,
+      ImgUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwXGlU2BC7XqXxhZiEg4Gp_WsCRxQkBsDdog&s',
+      CreatedAt: 'Test date 3',
+      Location: 'Rome, Italy',
+    },
+  ])
+}
