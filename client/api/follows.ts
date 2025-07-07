21bced446ab79e4157e38a81a28c0c230ebafa77
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-08 08:46:48 +1200

    Added - Client side api function to get accounts that follow a user

--- client/api/follows.ts
+++ client/api/follows.ts
@@ -6,3 +6,8 @@ export async function getUsersFollows(userId: number) {
   const result = await request.get(`${rootUrl}/following/${userId}`)
   return result.body
 }
+
+export async function getUsersFollowers(userId: number) {
+  const result = await request.get(`${rootUrl}/followers/${userId}`)
+  return result.body
+}
