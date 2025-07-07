2610f0e41ecdb8f91e50d7c2d513fa22242ca133
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-08 09:31:35 +1200

    Added - Hook function to get a certain posts info

--- client/hooks/usePosts.ts
+++ client/hooks/usePosts.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
-import { getPostsFromUser } from '../api/posts'
+import { getPostById, getPostsFromUser } from '../api/posts'
 
 export function useGetPostsFromUser(userId: number) {
   const query = useQuery({
     queryKey: ['posts from user: ', userId],
     queryFn: () => getPostsFromUser(userId),
+  })
+  return query
+}
+
+export function useGetPostById(postId: number) {
+  const query = useQuery({
+    queryKey: ['post: ', postId],
+    queryFn: () => getPostById(postId),
   })
   return query
 }
