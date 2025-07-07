cb2d7410543d0ee001780f4906feffc0a46f154b
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-08 09:15:39 +1200

    Fixed - Spelling of 'Conversations' on function name

--- client/api/conversations.ts
+++ client/api/conversations.ts
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1/conversations'
 
-export async function getAllCoversations() {
+export async function getAllConversations() {
   const result = await request.get(`${rootUrl}/all`)
   return result.body
 }
