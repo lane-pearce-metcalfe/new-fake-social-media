9e67d35a8fe9993ef4a4a2b598f912586d140df9
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-08 09:06:43 +1200

    Added - Client side api function to get certain posts data

--- client/api/posts.ts
+++ client/api/posts.ts
@@ -6,3 +6,8 @@ export async function getPostsFromUser(userId: number) {
   const result = await request.get(`${rootUrl}/user/${userId}`)
   return result.body
 }
+
+export async function getPostById(postId: number) {
+  const result = await request.get(`${rootUrl}/post/${postId}`)
+  return result.body
+}
