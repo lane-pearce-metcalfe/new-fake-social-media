72e11c0c3a232963e6878ca47576e816fd295cdd
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 08:50:11 +1200

    Added - Api route for getting all posts

--- server/db/routes/posts.ts
+++ server/db/routes/posts.ts
@@ -33,4 +33,15 @@ router.get('/post/:id', async (req, res) => {
   }
 })
 
+router.get('/all', async (req, res) => {
+  try {
+    const posts = await db.getAllPosts()
+
+    res.status(200).json(posts)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: `Something went wrong grabbing all posts` })
+  }
+})
+
 export default router
