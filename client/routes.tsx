1271e0f34e5ae85840f382c7ca42395fc6050eaf
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 08:48:18 +1200

    Updated - User page component added to routes

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,10 +1,12 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
 import PostPage from './components/PostPage.tsx'
 import Layout from './components/Layout.tsx'
+import UserPage from './components/UserPage.tsx'
 export default createRoutesFromElements(
   <>
     <Route path="/" element={<Layout />}>
       <Route path="post/:id" element={<PostPage />} />
+      <Route path="user/:id" element={<UserPage />} />
     </Route>
   </>,
 )
