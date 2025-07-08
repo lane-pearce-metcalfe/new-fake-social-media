ec8fc17e88f3965e5c05544f9f37a8a61ef78f5e
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 09:45:28 +1200

    Added - Api route for getting a user using Auth0 sub

--- server/db/routes/users.ts
+++ server/db/routes/users.ts
@@ -19,4 +19,19 @@ router.get('/id/:id', async (req, res) => {
   }
 })
 
+router.get('auth0/:sub', async (req, res) => {
+  const Auth0Sub = String(req.params.sub)
+
+  try {
+    const user = await db.getUserByAuth0Sub(Auth0Sub)
+
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: `Something went wrong grabbing user with Auth0Sub ${Auth0Sub}`,
+    })
+  }
+})
+
 export default router
