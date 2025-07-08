83550d4ce5bf731eeb72ccbfe98ab5672aba3133
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 09:36:55 +1200

    Updated - Header now allows users to sign in and out using Auth0

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,10 +1,30 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 export default function Header() {
+  const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0()
+
+  console.log(user)
+
+  function handleLogin() {
+    loginWithRedirect()
+  }
+
+  function handleLogout() {
+    logout()
+  }
+
   return (
     <header>
       <h1>Logo</h1>
       <div>
-        <p>Insert pfp</p>
-        <button>Sign in/out</button>
+        {!isAuthenticated ? (
+          <button onClick={handleLogin}>Sign in</button>
+        ) : (
+          <>
+            <button onClick={handleLogout}>Log Out</button>
+            <img src={user?.picture} alt={user?.nickname} />
+          </>
+        )}
       </div>
     </header>
   )
