e558228f9ca979bca76bc57a252feae6c5b2fb5a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 09:51:26 +1200

    Added - Hook function to search for a user using Auth0 sub

--- client/hooks/useUsers.ts
+++ client/hooks/useUsers.ts
@@ -1,10 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
-import { getUserById } from '../api/users'
+import { getUserByAuth0Sub, getUserById } from '../api/users'
 
 export function useGetUserById(userId: number) {
   const query = useQuery({
     queryKey: ['user: ', userId],
     queryFn: () => getUserById(userId),
+  })
+  return query
+}
+
+export function useGetUserByAuth0Sub(auth0Sub: string) {
+  const query = useQuery({
+    queryKey: ['auth0 sub: ', auth0Sub],
+    queryFn: () => getUserByAuth0Sub(auth0Sub),
   })
   return query
 }
