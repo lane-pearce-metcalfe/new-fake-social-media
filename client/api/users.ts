d92e8223a216134a156bf2c0796fe15f4c07bd31
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-09 09:49:49 +1200

    Added - Client side Api function to search for a user using Auth0 sub

--- client/api/users.ts
+++ client/api/users.ts
@@ -6,3 +6,8 @@ export async function getUserById(userId: number) {
   const result = await request.get(`${rootUrl}/id/${userId}`)
   return result.body
 }
+
+export async function getUserByAuth0Sub(auth0Sub: string) {
+  const result = await request.get(`${rootUrl}/auth0/${auth0Sub}`)
+  return result.body
+}
