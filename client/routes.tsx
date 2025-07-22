b4e767a3f7fba847e69017cc3c03ba61f333e4d4
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-23 11:04:58 +1200

    Updated - Added message box component to routes at '/conversation/:id'

--- client/routes.tsx
+++ client/routes.tsx
@@ -5,6 +5,7 @@ import UserPage from './components/UserPage.tsx'
 import HomeFeed from './components/HomeFeed.tsx'
 import LoadingUser from './components/LoadingUser.tsx'
 import ImageUploadComponent from './components/UploadPost.tsx'
+import MessageBox from './components/MessageBox.tsx'
 export default createRoutesFromElements(
   <>
     <Route path="/" element={<Layout />}>
@@ -12,6 +13,7 @@ export default createRoutesFromElements(
       <Route path="post/:id" element={<PostPage />} />
       <Route path="user/:id" element={<UserPage />} />
       <Route path="/upload" element={<ImageUploadComponent />} />
+      <Route path="conversation/:id" element={<MessageBox />} />
     </Route>
     <Route path="/loadUser" element={<LoadingUser />} />
   </>,
