36a52f8747bc837534c5dc3b298a894564db17e3
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-23 11:17:29 +1200

    Updated - Message box component now takes in an id from the urls params and then uses that id to get all the messages from that conversation

--- client/components/MessageBox.tsx
+++ client/components/MessageBox.tsx
@@ -1,3 +1,14 @@
+import { useGetConversationMessages } from '../hooks/useMessages'
+import { useParams } from 'react-router-dom'
+
 export default function MessageBox() {
+  const { id } = useParams()
+
+  const { data: conversationData } = useGetConversationMessages(Number(id))
+
+  if (!conversationData) {
+    return <p>Loading...</p>
+  }
+
   return <p>Testing...</p>
 }
