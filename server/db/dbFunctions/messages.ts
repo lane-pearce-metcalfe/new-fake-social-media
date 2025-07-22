dee1be666e90407c15ccd81cf972163f102b348a
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-23 10:57:22 +1200

    Added - Database function to add a message to the database

--- server/db/dbFunctions/messages.ts
+++ server/db/dbFunctions/messages.ts
@@ -1,6 +1,11 @@
+import { AddMessage } from '#models'
 import db from '../connection.ts'
 
 export async function getMessages(ConversationId: number) {
   const messages = db('messages').where({ ConversationId }).select()
   return messages
+}
+
+export async function addMessage(message: AddMessage) {
+  await db('messages').insert({ message })
 }
