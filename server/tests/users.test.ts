13d4e1094aa519e533db3d65b4f6f9730f2125c7
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-29 11:04:04 +1200

    Added - A test which should return a status code of 400 if a Auth0Id is missing which is currently failing

--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -32,4 +32,21 @@ describe('Adding a user', () => {
 
     expect(res.status).toBe(200)
   })
+
+  it('should fail adding a user due to missing auth0sub', async () => {
+    const userData = {
+      UserName: 'Testing Username',
+      FullName: 'Testing Fullname',
+      Email: 'testing@gmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: new Date().toISOString(),
+    }
+
+    const res = await request(server).post(`/api/v1/users`).send(userData)
+
+    console.log(res.status)
+
+    expect(res.status).toBe(400)
+  })
 })
