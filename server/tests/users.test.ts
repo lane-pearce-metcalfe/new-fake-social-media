5d6e305338bf7e6d34a45fb6c947b1c5d94e3736
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-29 14:58:57 +1200

    Updated - Fake user data object is now declared at the top level of the test and when passing it in for test it just uses the spread operator and changes data as needed

--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -17,80 +17,50 @@ afterAll(async () => {
 })
 
 describe('Adding a user', () => {
-  it('should add a user successfully', async () => {
-    const userData = {
-      UserName: 'Testing Username',
-      FullName: 'Testing Fullname',
-      Email: 'testing@gmail.com',
-      PfpUrl:
-        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
-      UserSince: new Date().toISOString(),
-      Auth0Sub: 'auth0|test123',
-    }
+  const userData = {
+    UserName: 'Testing Username',
+    FullName: 'Testing Fullname',
+    Email: 'testing@gmail.com',
+    PfpUrl:
+      'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+    UserSince: new Date().toISOString(),
+    Auth0Sub: 'auth0|test123',
+  }
 
+  it('should add a user successfully', async () => {
     const res = await request(server).post('/api/v1/users').send(userData)
 
     expect(res.status).toBe(200)
   })
 
   it('should fail adding a user due to missing auth0sub', async () => {
-    const userData = {
-      UserName: 'Testing Username',
-      FullName: 'Testing Fullname',
-      Email: 'testing@gmail.com',
-      PfpUrl:
-        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
-      UserSince: new Date().toISOString(),
-    }
-
-    const res = await request(server).post(`/api/v1/users`).send(userData)
+    const res = await request(server)
+      .post(`/api/v1/users`)
+      .send({ ...userData, Auth0Sub: null })
 
     expect(res.status).toBe(400)
   })
 
   it('should fail adding a user to missing email', async () => {
-    const userData = {
-      UserName: 'Testing Username',
-      FullName: 'Testing Fullname',
-      PfpUrl:
-        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
-      UserSince: new Date().toISOString(),
-      Auth0Sub: 'auth0|test123',
-    }
-
-    const res = await request(server).post(`/api/v1/users`).send(userData)
+    const res = await request(server)
+      .post(`/api/v1/users`)
+      .send({ ...userData, Email: null })
 
     expect(res.status).toBe(400)
   })
 
   it('should reject a invalid email format (doesnt include an "@")', async () => {
-    const userData = {
-      UserName: 'Testing Username',
-      FullName: 'Testing Fullname',
-      Email: 'testinggmail.com',
-      PfpUrl:
-        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
-      UserSince: new Date().toISOString(),
-      Auth0Sub: 'auth0|test123',
-    }
-
-    const res = await request(server).post(`/api/v1/users`).send(userData)
+    const res = await request(server)
+      .post(`/api/v1/users`)
+      .send({ ...userData, Email: 'testemail.com' })
 
     expect(res.status).toBe(400)
   })
 
   it('should reject an invalid email format (not enough characters)', async () => {
-    const userData = {
-      UserName: 'Testing Username',
-      FullName: 'Testing Fullname',
-      Email: '@g.co',
-      PfpUrl:
-        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
-      UserSince: new Date().toISOString(),
-      Auth0Sub: 'auth0|test123',
-    }
-
-    const res = await request(server).post(`/api/v1/users`).send(userData)
+    const res = await request(server)
+      .post(`/api/v1/users`)
+      .send({ ...userData, Email: '@g.co' })
 
     expect(res.status).toBe(400)
   })
