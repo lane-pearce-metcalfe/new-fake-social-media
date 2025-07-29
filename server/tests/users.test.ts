26c1789725b75f8a89439e639a56ae03ebf9f683
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-29 14:22:53 +1200

    Added - A test to see if an invalid email because of a lack of chracters will be rejected

--- server/tests/users.test.ts
+++ server/tests/users.test.ts
@@ -63,11 +63,27 @@ describe('Adding a user', () => {
     expect(res.status).toBe(400)
   })
 
-  it('should reject a invalid email format', async () => {
+  it('should reject a invalid email format (doesnt include an "@")', async () => {
     const userData = {
       UserName: 'Testing Username',
       FullName: 'Testing Fullname',
       Email: 'testinggmail.com',
+      PfpUrl:
+        'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
+      UserSince: new Date().toISOString(),
+      Auth0Sub: 'auth0|test123',
+    }
+
+    const res = await request(server).post(`/api/v1/users`).send(userData)
+
+    expect(res.status).toBe(400)
+  })
+
+  it('should reject an invalid email format (not enough characters)', async () => {
+    const userData = {
+      UserName: 'Testing Username',
+      FullName: 'Testing Fullname',
+      Email: '@g.co',
       PfpUrl:
         'https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg',
       UserSince: new Date().toISOString(),
