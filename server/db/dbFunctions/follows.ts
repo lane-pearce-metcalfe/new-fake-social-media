444db7a941adb749d32424ab19baec8d754fafde
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-30 13:36:52 +1200

    Added - Database function to find the following relationship between 2 users

--- server/db/dbFunctions/follows.ts
+++ server/db/dbFunctions/follows.ts
@@ -9,3 +9,28 @@ export async function getUsersFollowers(FollowedUserId: number) {
   const followers = await db('follows').where({ FollowedUserId }).select()
   return followers
 }
+
+type Relationship = 'none' | 'following' | 'followed' | 'mutual'
+
+export async function getRelationship(
+  UserId: number,
+  FollowedUserId: number,
+): Promise<Relationship> {
+  const following = await db('follows')
+    .where({ UserId, FollowedUserId })
+    .first()
+
+  const followed = await db('follows')
+    .where({ UserId: FollowedUserId, FollowedUserId: UserId })
+    .first()
+
+  if (following && followed) {
+    return 'mutual'
+  } else if (following) {
+    return 'following'
+  } else if (followed) {
+    return 'followed'
+  } else {
+    return 'none'
+  }
+}
