f7060904a938ba80cf02ab069c4e30f074fe3e2d
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-31 10:20:59 +1200

    Added - Hook function to get the relationship between 2 users

--- client/hooks/useFollows.ts
+++ client/hooks/useFollows.ts
@@ -1,5 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import { getUsersFollowers, getUsersFollows } from '../api/follows'
+import {
+  getUsersFollowers,
+  getUsersFollows,
+  getRelationship,
+} from '../api/follows'
 
 export function useGetUsersFollows(userId: number) {
   const query = useQuery({
@@ -13,6 +17,14 @@ export function useGetUsersFollowers(userId: number) {
   const query = useQuery({
     queryKey: ['followers for user ', userId],
     queryFn: () => getUsersFollowers(userId),
+  })
+  return query
+}
+
+export function useGetRelationships(userId: number, followedUserId: number) {
+  const query = useQuery({
+    queryKey: ['relationship for user', userId, followedUserId],
+    queryFn: () => getRelationship(userId, followedUserId),
   })
   return query
 }
