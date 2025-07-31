eee463f4a609cc5b63ff33f3f30f7d10f9773eb9
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-07-31 14:42:30 +1200

    Updated - Changed it so that theres a single api call to get both the amount of followers and follows a user has

--- client/api/follows.ts
+++ client/api/follows.ts
@@ -2,14 +2,13 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1/follows'
 
-export async function getUsersFollows(userId: number) {
-  const result = await request.get(`${rootUrl}/following/${userId}`)
-  return result.body
-}
-
-export async function getUsersFollowers(userId: number) {
-  const result = await request.get(`${rootUrl}/followers/${userId}`)
-  return result.body
+export async function getUsersFollowCounts(userId: number) {
+  const follows = await request.get(`${rootUrl}/following/${userId}`)
+  const followers = await request.get(`${rootUrl}/followers/${userId}`)
+  return {
+    follows: follows.body,
+    followers: followers.body,
+  }
 }
 
 export async function getRelationship(userId: number, followedUserId: number) {
